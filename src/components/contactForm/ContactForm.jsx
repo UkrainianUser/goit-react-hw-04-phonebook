6ebf358657c750e0732d85c19e939b23bb632a11
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-04-30 16:02:16 +0300

    change ContactForm to hooks

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,38 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import css from "./ContactForm.module.css";
 import PropTypes from 'prop-types';
 
-class ContactForm extends React.Component {
+export default function ContactForm ({onSubmit}) {
 
-	state = {
-		name: '',
-    number: ''
-	};
+	const [name, setName] = useState('');
+	const [number, setNumber] = useState('');
 	
-	static propTypes = {
-		onSubmit: PropTypes.func.isRequired,
-  };
-
-	handleChange = evt => {
+	const handleChange = evt => {
     const { name, value } = evt.currentTarget;
-    this.setState({[name]: value,});
-  };
-
-	handleSubmit = evt => {
-    evt.preventDefault();
-		this.props.onSubmit(this.state);
-		this.reset();
-  };
-
-	reset = () => {
-		this.setState({
-			name: '',
-			number: ''
-		})
+    if (name === "name") {
+      setName(value);
+    } else if (name === "number") {
+      setNumber(value);
+    }
+};
+	
+	const reset = () => {
+		setName('');
+		setNumber('');
 	};
 
-	render(){
-	return	<form className={css.form} onSubmit={this.handleSubmit}>
+	const handleSubmit = evt => {
+    evt.preventDefault();
+		onSubmit({name, number});
+		reset();
+  };
+
+	return	<form className={css.form} onSubmit={handleSubmit}>
 		<label className={css.label}>
 			Name 
 			<input
@@ -42,8 +37,8 @@ class ContactForm extends React.Component {
 				pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
 				title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
 				required
-				value={this.state.name} 
-				onChange={this.handleChange}
+				value={name} 
+				onChange={handleChange}
 			/>
 		</label>
 		<label className={css.label}>
@@ -55,13 +50,14 @@ class ContactForm extends React.Component {
 				pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
 				title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
 				required
-				value={this.state.number} 
-				onChange={this.handleChange}
+				value={number} 
+				onChange={handleChange}
 			/>
 		</label>
 		<button className={css.formBtn} type="submit">Add contact</button>
 		</form>
-	}	
 };
 
-export default ContactForm;
+ContactForm.propTypes = {
+	onSubmit: PropTypes.func.isRequired,
+};
